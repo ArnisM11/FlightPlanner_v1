7ba2ae77c55d93dbe8abd52d33b66f458f9d9b70
Arnis Mednis <119369248+ArnisM11@users.noreply.github.com>
2023-05-08 17:26:53 +0300

    Add project files.

---
+++ Controllers/AdminApiController.cs
@@ -0,0 +1,36 @@
+﻿using FlightPlanner.Models;
+using FlightPlanner.Storage;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+
+    [Route("admin-api")]
+    [ApiController]
+    [Authorize]
+    public class AdminApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("flights/{id}")]
+        public IActionResult GetFlight(int id)
+        {
+            var flight = FlightStorage.GetFlight(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            return Ok(flight);
+        }
+        [HttpPut]
+        [Route("flights")]
+
+        public IActionResult AddFlight(Flight flight)
+        {
+            FlightStorage.AddFlight(flight);
+            return Created("",flight);
+        }
+
+
+    }
+}
---
+++ Storage/FlightStorage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FlightPlanner.Models;
+
+namespace FlightPlanner.Storage
+{
+    public static class FlightStorage
+    {
+        private static List<Flight> _flights = new List<Flight>();
+        private static int _id = 1;
+
+        //make GetFlight,SaveFlight
+
+        public static Flight GetFlight(int id)
+        {
+            return _flights.SingleOrDefault(flight => flight.Id == id);
+        }
+
+        public static Flight AddFlight(Flight flight)
+        {
+            flight.Id = _id++;
+            _flights.Add(flight);
+            return flight;
+        }
+    }
+}
